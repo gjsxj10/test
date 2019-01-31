c4d5753cd01731cc330e72122462bde1cf64cc87
Lsanta <38778788+Lsanta@users.noreply.github.com>
2019-01-31 10:29:08 +0900

    q2

--- wdb/src/wdb/Main.java
+++ wdb/src/wdb/Main.java
@@ -2,7 +2,7 @@ package wdb;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("3+5=?");
-		System.out.println("8");
+		System.out.println("3+6=?");
+		System.out.println("?안에 정답을 채워넣어서 결과창에 출력되게 해주세요");
 	}
 }
