8027e91e3f6758187f6e91a0e08c9517e4e386af
kdn1391 <46514429+kdn1391@users.noreply.github.com>
2019-01-31 10:22:06 +0900

    Update Main.java

--- wdb/src/wdb/Main.java
+++ wdb/src/wdb/Main.java
@@ -3,6 +3,6 @@ package wdb;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("3+5=?");
-		System.out.println("?에 들어갈 숫자 실행결과에 같이 나오게 바꿔주세요");
+		System.out.println("8");
 	}
 }
