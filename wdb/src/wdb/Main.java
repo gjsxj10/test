4df8b19fb289d1b54dc7a601f3ecdf50601bd465
Lsanta <38778788+Lsanta@users.noreply.github.com>
2019-01-31 10:11:06 +0900

    q1

--- wdb/src/wdb/Main.java
+++ wdb/src/wdb/Main.java
@@ -2,6 +2,7 @@ package wdb;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("gd");
+		System.out.println("3+5=?");
+		System.out.println("?에 들어갈 숫자 실행결과에 같이 나오게 바꿔주세요");
 	}
 }
